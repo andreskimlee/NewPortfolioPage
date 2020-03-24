588d06645d314345f66bc13c2a413e8ebc014ab9
Andres Kim <andreskimlee@gmail.com>
2020-03-24 16:06:29 -0400

    First Commit

---
+++ src/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+class NavBar extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            loaded: false
+        }
+    }
+
+    render() {
+        return (
+            <div class="NavContainer">
+
+            </div>
+        )
+    }
+}
+
+export default NavBar;
